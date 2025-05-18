babc54093756a08aa5000ba4aa72077179743e18
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-18 11:07:44 +0100

    Added customers and accounts components

---
+++ src/app/accounts/accounts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts',
+  standalone: false,
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomersComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
