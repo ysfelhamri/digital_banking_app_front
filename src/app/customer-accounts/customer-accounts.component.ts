4cd999ed3d7d146fc319492ae07336dce4145c10
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-18 19:05:43 +0100

    Added customer-accounts component

---
+++ src/app/customer-accounts/customer-accounts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-accounts',
+  standalone: false,
+  templateUrl: './customer-accounts.component.html',
+  styleUrl: './customer-accounts.component.css'
+})
+export class CustomerAccountsComponent {
+
+}
