c4cc9edc9ae2bd46cf33a3dcc4321465f873a277
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-18 19:13:27 +0100

    Added search and account operations for Accounts

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Customer} from '../model/customer.model';
 import {environment} from '../../environments/environment';
+import {AccountDetails} from '../model/account.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http : HttpClient) { }
 
-  public getCustomers():Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(environment.backendHost+"/customers")
+  public getAccount(accountId : string, page : number, size : number):Observable<AccountDetails>{
+    return this.http.get<AccountDetails>(environment.backendHost+"/accounts/"+accountId+"/pageOperations?page="+page+"&size="+size);
   }
-  public searchCustomers(keyword : string):Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(environment.backendHost+"/customers/search?keyword="+keyword)
+  public debit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(environment.backendHost+"/accounts/debit",data);
   }
-  public saveCustomer(customer: Customer):Observable<Customer>{
-    return this.http.post<Customer>(environment.backendHost+"/customers",customer);
+  public credit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(environment.backendHost+"/accounts/credit",data);
   }
-  public deleteCustomer(id: number){
-    return this.http.delete(environment.backendHost+"/customers/"+id);
+  public transfer(accountSource: string,accountDestination: string, amount : number, description:string){
+    let data={accountSource, accountDestination, amount, description }
+    return this.http.post(environment.backendHost+"/accounts/transfer",data);
   }
 }
