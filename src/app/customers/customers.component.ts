b675aaf12ada9c31dae5b44dce39fc5ec34b9b7c
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-18 11:26:55 +0100

    Added basic data retrieval from the backend server for customers

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-customers',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+  customers : any;
+  constructor(private http:HttpClient) {
+  }
 
+  ngOnInit(): void {
+    this.http.get("http://localhost:8085/customers").subscribe({
+      next : (data)=>{
+        this.customers = data
+      },
+      error : (err)=>{
+        console.log(err)
+      }
+    })
+  }
 }
