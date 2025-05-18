79fb6ed0eb9d0cffb3dff3e054c18e4ba7d96b4e
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-18 17:25:35 +0100

    Added new-customer component

---
+++ src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-customer',
+  standalone: false,
+  templateUrl: './new-customer.component.html',
+  styleUrl: './new-customer.component.css'
+})
+export class NewCustomerComponent {
+
+}
